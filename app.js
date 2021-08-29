5244745809bee1cf2ddcf5a689cea9f806a55742
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-08-29 17:56:58 +0530

    removed frontend

--- app.js
+++ app.js
@@ -1,52 +1,25 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
 const locationKey = require('./utils/location');
 const forecast = require('./utils/forecast');
-require('dotenv').config();
 
-const app = express();
-const port = 3000;
+const cityName = process.argv[2];
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+if (!cityName) {
+    console.log('Please provide city name.');
+} else {
 
+    locationKey(cityName, (error, data) => {
+        if (error) {
+            console.log(`Error: ${error}`);
+        } else {
+            console.log(`Location Key : ${data}`);
 
-
-app.get('/', (req, res) => {
-    res.render('weather');
-});
-
-app.post('/', (req, res) => {
-
-    const cityName = req.body.city;
-
-    if (!cityName) {
-        console.log('Please provide city name.');
-    } else {
-
-        locationKey(cityName, (error, data) => {
-            if (error) {
-                console.log(`Error: ${error}`);
-            } else {
-                console.log(data);
-
-                forecast(data, (error, forecastData) => {
-                    if (error) {
-                        console.log(`Error: ${error}`);
-                    } else {
-                        console.log(forecastData);
-                    }
-                });
-            }
-        });
-    }
-
-    res.redirect('/');
-
-});
-
-app.listen(process.env.PORT || port, function() {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+            forecast(data, (error, forecastData) => {
+                if (error) {
+                    console.log(`Error: ${error}`);
+                } else {
+                    console.log(forecastData);
+                }
+            });
+        }
+    });
+}
